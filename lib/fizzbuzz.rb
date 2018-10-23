495fa491e3defd48427755cac6a94ffdc8529aae
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-10-23 15:34:33 +0100

    added prime number test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,10 @@
 def fizzbuzz(number)
-if number % 15 == 0
-  return 'fizzbuzz'
-elsif number % 5 == 0
-  return 'buzz'
-elsif number % 3 == 0
-  return 'fizz'
-end
+  if number % 15 == 0
+    return 'fizzbuzz'
+  elsif number % 5 == 0
+    return 'buzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  end
+  return number
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/fizzbuzz'
+require "Prime"
 
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
@@ -19,7 +20,22 @@ describe 'fizzbuzz' do
 end
 
 describe 'fizzbuzz' do
-  it 'returns "fizzbuzz" when passed a multiple of 15' do
+  it 'returns "fizzbuzz" when passed a multiple of 3' do
     expect(fizzbuzz((1+rand(4))*3)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "8" when passed 8' do
+    expect(fizzbuzz(8)).to eq 8
+  end
+end
+
+describe 'fizbuzz' do
+  it 'returns that prime number under 100 when passed that prime number' do
+    Prime.each(100) do |prime|
+      next if prime == 3 || prime == 5
+      expect(fizzbuzz(prime)).to eq prime
+    end
   end
 end
